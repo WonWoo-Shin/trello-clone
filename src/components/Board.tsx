c2364b673a171003c2da4129795d9f0182c55675
WonWoo-Shin <sww0481@gmail.com>
2024-05-25 20:16:36 +0900

    board 이름 변경 기능 추가

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,23 +1,48 @@
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import { BoardBlock, BoardContainer, BoardTitle, Cards } from "../style/style";
-import { memo } from "react";
+import { memo, useState } from "react";
 import DraggableCard from "./DraggableCard";
-import { ICard, TDefaultBoard } from "../atom";
+import { ICard, boardOrderState, boardsState } from "../atom";
 import AddCard from "./AddCard";
+import { useSetRecoilState } from "recoil";
 
 interface IBoardProps {
-  boardId: TDefaultBoard;
+  boardId: string;
   cards: ICard[];
   index: number;
 }
 
 function Board({ boardId, cards, index }: IBoardProps) {
+  const [text, setText] = useState(boardId);
+  const setBoards = useSetRecoilState(boardsState);
+  const setBoardOrder = useSetRecoilState(boardOrderState);
+  const changeBoardName = () => {
+    if (boardId === text) return;
+    setBoardOrder((oldOrder) => {
+      const targetIndex = oldOrder.indexOf(boardId);
+      const newOrder = [...oldOrder];
+      newOrder.splice(targetIndex, 1, text);
+      return newOrder;
+    });
+    setBoards((oldBoards) => {
+      const newBoards = { ...oldBoards };
+      newBoards[text] = newBoards[boardId];
+      delete newBoards[boardId];
+      return newBoards;
+    });
+  };
   return (
     <Draggable draggableId={boardId} index={index}>
       {(provided) => (
         <BoardBlock ref={provided.innerRef} {...provided.draggableProps}>
           <BoardContainer>
             <BoardTitle {...provided.dragHandleProps}>{boardId}</BoardTitle>
+            <input
+              type="text"
+              value={text}
+              onChange={(event) => setText(event.currentTarget.value)}
+              onBlur={changeBoardName}
+            />
             <Droppable droppableId={boardId}>
               {(provided) => (
                 <Cards ref={provided.innerRef} {...provided.droppableProps}>
