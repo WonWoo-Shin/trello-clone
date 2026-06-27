f22d5adb94fbc8d7445e78cc34a1e21d61492349
WonWoo-Shin <sww0481@gmail.com>
2026-06-27 22:04:39 +0900

    Boards 표시 zustand 변경

--- src/components/Boards.tsx
+++ src/components/Boards.tsx
@@ -8,136 +8,139 @@ import invariant from "tiny-invariant";
 import { extractClosestEdge } from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge";
 import { AddBoard } from "./AddBoard";
 import { Board } from "./Board";
+import { useBoardStore } from "../store/useBoardStore";
 
 export const Boards = () => {
-  const [boards, setBoards] = useRecoilState(boardsState);
-  const [boardOrder, setBoardOrder] = useRecoilState(boardOrderState);
+  // const [boards, setBoards] = useRecoilState(boardsState);
+  // const [boardOrder, setBoardOrder] = useRecoilState(boardOrderState);
+
+  const { boardOrder, boards } = useBoardStore();
 
   const [isAddOpen, setIsAddOpen] = useState(false);
 
   //element monitor
-  useEffect(() => {
-    return monitorForElements({
-      onDragStart: () => {},
-      onDrop: ({ source, location }) => {
-        if (!location.current.dropTargets.length) {
-          //drop outside
-          return;
-        }
-        const target = location.current.dropTargets[0];
-        if (target.element !== location.initial.dropTargets[0].element) {
-          //move item
-          const sourceBoardId = source.data.boardId;
-          const targetBoardId = target.data.boardId;
-          invariant(typeof sourceBoardId === "number");
-          invariant(typeof targetBoardId === "number");
+  // useEffect(() => {
+  //   return monitorForElements({
+  //     onDragStart: () => {},
+  //     onDrop: ({ source, location }) => {
+  //       if (!location.current.dropTargets.length) {
+  //         //drop outside
+  //         return;
+  //       }
+  //       const target = location.current.dropTargets[0];
+  //       if (target.element !== location.initial.dropTargets[0].element) {
+  //         //move item
+  //         const sourceBoardId = source.data.boardId;
+  //         const targetBoardId = target.data.boardId;
+  //         invariant(typeof sourceBoardId === "number");
+  //         invariant(typeof targetBoardId === "number");
 
-          switch (source.data.type) {
-            case "board":
-              const currentClosetEdge = extractClosestEdge(target.data);
-              setBoardOrder((order) => {
-                const sourceIndex = order.findIndex(
-                  (item) => item === sourceBoardId,
-                );
-                let targetIndex = order.findIndex(
-                  (item) => item === targetBoardId,
-                );
+  //         switch (source.data.type) {
+  //           case "board":
+  //             const currentClosetEdge = extractClosestEdge(target.data);
+  //             setBoardOrder((order) => {
+  //               const sourceIndex = order.findIndex(
+  //                 (item) => item === sourceBoardId,
+  //               );
+  //               let targetIndex = order.findIndex(
+  //                 (item) => item === targetBoardId,
+  //               );
 
-                const isMovingLeft = sourceIndex > targetIndex;
-                const isMovingRight = sourceIndex < targetIndex;
-                if (isMovingRight && currentClosetEdge === "left") {
-                  targetIndex -= 1;
-                } else if (isMovingLeft && currentClosetEdge === "right") {
-                  targetIndex += 1;
-                }
+  //               const isMovingLeft = sourceIndex > targetIndex;
+  //               const isMovingRight = sourceIndex < targetIndex;
+  //               if (isMovingRight && currentClosetEdge === "left") {
+  //                 targetIndex -= 1;
+  //               } else if (isMovingLeft && currentClosetEdge === "right") {
+  //                 targetIndex += 1;
+  //               }
 
-                const newOrder = [...order];
-                const draggedItem = newOrder.splice(sourceIndex, 1);
-                newOrder.splice(targetIndex, 0, ...draggedItem);
+  //               const newOrder = [...order];
+  //               const draggedItem = newOrder.splice(sourceIndex, 1);
+  //               newOrder.splice(targetIndex, 0, ...draggedItem);
 
-                return newOrder;
-              });
-              break;
+  //               return newOrder;
+  //             });
+  //             break;
 
-            case "card":
-              const targetType = target.data.type;
-              const sourceCardId = source.data.cardId;
-              const targetCardId = target.data.cardId;
+  //           case "card":
+  //             const targetType = target.data.type;
+  //             const sourceCardId = source.data.cardId;
+  //             const targetCardId = target.data.cardId;
 
-              const sourceIndex = boards[sourceBoardId].cards.findIndex(
-                (item) => item.cardId === sourceCardId,
-              );
-              let targetIndex = boards[targetBoardId].cards.findIndex(
-                (item) => item.cardId === targetCardId,
-              );
+  //             const sourceIndex = boards[sourceBoardId].cards.findIndex(
+  //               (item) => item.cardId === sourceCardId,
+  //             );
+  //             let targetIndex = boards[targetBoardId].cards.findIndex(
+  //               (item) => item.cardId === targetCardId,
+  //             );
 
-              if (sourceBoardId === targetBoardId) {
-                //same board
-                setBoards((boards) => {
-                  const currentClosetEdge = extractClosestEdge(target.data);
-                  const isMovingUp = sourceIndex > targetIndex;
-                  const isMovingDown = sourceIndex < targetIndex;
-                  if (isMovingUp && currentClosetEdge === "bottom") {
-                    targetIndex += 1;
-                  }
-                  if (isMovingDown && currentClosetEdge === "top") {
-                    targetIndex -= 1;
-                  }
+  //             if (sourceBoardId === targetBoardId) {
+  //               //same board
+  //               setBoards((boards) => {
+  //                 const currentClosetEdge = extractClosestEdge(target.data);
+  //                 const isMovingUp = sourceIndex > targetIndex;
+  //                 const isMovingDown = sourceIndex < targetIndex;
+  //                 if (isMovingUp && currentClosetEdge === "bottom") {
+  //                   targetIndex += 1;
+  //                 }
+  //                 if (isMovingDown && currentClosetEdge === "top") {
+  //                   targetIndex -= 1;
+  //                 }
 
-                  const copyCards = [...boards[sourceBoardId].cards];
-                  const draggedCard = copyCards.splice(sourceIndex, 1);
-                  if (targetType === "card") {
-                    copyCards.splice(targetIndex, 0, ...draggedCard);
-                  } else if (targetType === "board") {
-                    copyCards.push(...draggedCard);
-                  }
+  //                 const copyCards = [...boards[sourceBoardId].cards];
+  //                 const draggedCard = copyCards.splice(sourceIndex, 1);
+  //                 if (targetType === "card") {
+  //                   copyCards.splice(targetIndex, 0, ...draggedCard);
+  //                 } else if (targetType === "board") {
+  //                   copyCards.push(...draggedCard);
+  //                 }
 
-                  return {
-                    ...boards,
-                    [sourceBoardId]: {
-                      ...boards[sourceBoardId],
-                      cards: copyCards,
-                    },
-                  };
-                });
-              } else if (sourceBoardId !== targetBoardId) {
-                //cross board
-                setBoards((boards) => {
-                  const currentClosetEdge = extractClosestEdge(target.data);
-                  if (currentClosetEdge === "bottom") {
-                    targetIndex += 1;
-                  }
+  //                 return {
+  //                   ...boards,
+  //                   [sourceBoardId]: {
+  //                     ...boards[sourceBoardId],
+  //                     cards: copyCards,
+  //                   },
+  //                 };
+  //               });
+  //             } else if (sourceBoardId !== targetBoardId) {
+  //               //cross board
+  //               setBoards((boards) => {
+  //                 const currentClosetEdge = extractClosestEdge(target.data);
+  //                 if (currentClosetEdge === "bottom") {
+  //                   targetIndex += 1;
+  //                 }
 
-                  const copySourceCards = [...boards[sourceBoardId].cards];
-                  const copyTargetCards = [...boards[targetBoardId].cards];
-                  const draggedCard = copySourceCards.splice(sourceIndex, 1);
-                  if (targetType === "card") {
-                    copyTargetCards.splice(targetIndex, 0, ...draggedCard);
-                  } else if (targetType === "board") {
-                    copyTargetCards.push(...draggedCard);
-                  }
+  //                 const copySourceCards = [...boards[sourceBoardId].cards];
+  //                 const copyTargetCards = [...boards[targetBoardId].cards];
+  //                 const draggedCard = copySourceCards.splice(sourceIndex, 1);
+  //                 if (targetType === "card") {
+  //                   copyTargetCards.splice(targetIndex, 0, ...draggedCard);
+  //                 } else if (targetType === "board") {
+  //                   copyTargetCards.push(...draggedCard);
+  //                 }
 
-                  return {
-                    ...boards,
-                    [sourceBoardId]: {
-                      ...boards[sourceBoardId],
-                      cards: copySourceCards,
-                    },
-                    [targetBoardId]: {
-                      ...boards[targetBoardId],
-                      cards: copyTargetCards,
-                    },
-                  };
-                });
-              }
-              break;
-            default:
-              return;
-          }
-        }
-      },
-    });
-  }, [boards, boardOrder]);
+  //                 return {
+  //                   ...boards,
+  //                   [sourceBoardId]: {
+  //                     ...boards[sourceBoardId],
+  //                     cards: copySourceCards,
+  //                   },
+  //                   [targetBoardId]: {
+  //                     ...boards[targetBoardId],
+  //                     cards: copyTargetCards,
+  //                   },
+  //                 };
+  //               });
+  //             }
+  //             break;
+  //           default:
+  //             return;
+  //         }
+  //       }
+  //     },
+  //   });
+  // }, [boards, boardOrder]);
 
   return (
     <BoardsList>
