c9132e75b62c06895272ac3820189537bfc2de0a
WonWoo-Shin <sww0481@gmail.com>
2026-06-27 21:26:33 +0900

    zustand 기본 세팅

---
+++ src/store/useBoardStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+
+interface ICard {
+  cardId: number;
+  cardText: string;
+  cardCheck: boolean;
+}
+
+interface IBoards {
+  [key: number]: {
+    boardName: string;
+    cards: ICard[];
+  };
+}
+
+interface IBoardState {
+  boardOrder: number[];
+  boards: IBoards;
+}
+
+export const useBoardStore = create<IBoardState>((set) => ({
+  boardOrder: [123456789, 123456790, 123456791],
+  boards: {
+    123456789: {
+      boardName: "To do",
+      cards: [
+        { cardId: 12345678, cardText: "Project planning", cardCheck: true },
+        { cardId: 12345679, cardText: "b", cardCheck: false },
+        { cardId: 12345680, cardText: "c", cardCheck: false },
+      ],
+    },
+    123456790: {
+      boardName: "Doing",
+      cards: [],
+    },
+    123456791: {
+      boardName: "Done",
+      cards: [],
+    },
+  },
+}));
